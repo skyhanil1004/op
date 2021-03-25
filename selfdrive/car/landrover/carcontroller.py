4ea9c085bef7301f1a944886b947d108645c69c7
Comma Device <device@comma.ai>
2021-03-25 13:01:39 +0000

    land_hanil #2

--- selfdrive/car/landrover/carcontroller.py
+++ selfdrive/car/landrover/carcontroller.py
@@ -6,13 +6,18 @@ from opendbc.can.packer import CANPacker
 from selfdrive.swaglog import cloudlog
 
 
-AudibleAlert = car.CarControl.HUDControl.AudibleAlert
-LOUD_ALERTS = [AudibleAlert.chimeWarning1, AudibleAlert.chimeWarning2, AudibleAlert.chimeWarningRepeat]
-
 class CarController():
-  def __init__(self, dbc_name, car_fingerprint, enable_camera):
+  def __init__(self, dbc_name, CP, VM):
     self.braking = False
     # redundant safety check with the board
+    self.apply_steer_last = 0
+    self.ccframe = 0
+    self.prev_frame = -1
+    self.hud_count = 0
+    self.car_fingerprint = CP.carFingerprint
+    self.gone_fast_yet = False
+    self.steer_rate_limited = False
+
     self.controls_allowed = True
     self.apply_steer_last = 0
     self.ccframe = 0
@@ -24,37 +29,20 @@ class CarController():
     self.send_chime = False
     self.gone_fast_yet = False
 
-    self.fake_ecus = set()
-    if enable_camera:
-      self.fake_ecus.add(ECU.CAM)
-
     self.packer = CANPacker(dbc_name)
 
-  def update(self, enabled, CS, frame, actuators, pcm_cancel_cmd, hud_alert, 
-             left_line, right_line, left_lane_depart, right_lane_depart):
-
-    """
-    # init safety test lines
-    if CS.generic_toggle:
-        actuators.steer = 1
-    apply_steer = int(round(actuators.steer * SteerLimitParams.STEER_MAX))
-    # cloudlog.warning("LANDROVER LKAS aplly_steer(%d)", actuators.steer)
-    # end safety test line
-    """
-
-    # *** compute control surfaces ***
-    # steer torque
+  def update(self, enabled, CS, frame, actuators, pcm_cancel_cmd, hud_alert) 
 
     apply_steer = actuators.steer * SteerLimitParams.STEER_MAX
     #apply_std_steer_torque_limits(apply_steer, self.apply_stee    r_last, CS.steer_torque_driver, SteerLimitParams)
-    apply_steer = apply_toyota_steer_torque_limits(apply_steer, self.apply_steer_last, CS.steer_torque_motor, SteerLimitParams)
+    apply_steer = apply_toyota_steer_torque_limits(apply_steer, self.apply_steer_last, CS.out.steeringTorqueEps, SteerLimitParams)
 
     # """
-    moving_fast = CS.v_ego > CS.CP.minSteerSpeed  # for status message
-    if CS.v_ego > (CS.CP.minSteerSpeed - 0.5):  # for command high bit
+    moving_fast = CS.out.vEgo > CS.CP.minSteerSpeed  # for status message
+    if CS.out.vEgo > (CS.CP.minSteerSpeed - 0.5):  # for command high bit
       self.gone_fast_yet = True
     elif self.car_fingerprint in (CAR.RANGEROVER_2017_VOGUE):
-      if CS.v_ego < (CS.CP.minSteerSpeed - 3.0):
+      if CS.out.vEgo < (CS.CP.minSteerSpeed - 3.0):
         self.gone_fast_yet = False  # < 14.5m/s stock turns off this bit, but fine down to 13.5
     lkas_active = moving_fast and enabled
     # cloudlog.warning("LANDROVER LKAS aplly_steer(%d)%d", apply_steer, actuators.steer)
@@ -69,42 +57,7 @@ class CarController():
 
     self.apply_steer_last = apply_steer
 
-    """
-    if audible_alert in LOUD_ALERTS:
-      self.send_chime = True
-    """
-    
     can_sends = []
-
-    #*** control msgs ***
-
-    """
-    if self.send_chime:
-      new_msg = create_chimes(AudibleAlert)
-      can_sends.append(new_msg)
-      if audible_alert not in LOUD_ALERTS:
-        self.send_chime = False
-    """
-
-    """
-    if pcm_cancel_cmd:
-      # TODO: would be better to start from frame_2b3
-      new_msg = create_wheel_buttons(self.ccframe)
-      can_sends.append(new_msg)
-    """
-
-    # LKAS_HEARTBIT is forwarded by Panda so no need to send it here.
-    # frame is 100Hz (0.01s period)
-
-    """
-    if (self.ccframe % 25 == 0):  # 0.25s period
-      if (CS.lkas_car_model != -1):
-        new_msg = create_lkas_hud(
-            self.packer, CS.gear_shifter, lkas_active, hud_alert,
-            self.hud_count, CS.lkas_car_model)
-        can_sends.append(new_msg)
-        self.hud_count += 1
-    """
 
     for (addr, ecu, cars, bus, fr_step, vl) in STATIC_MSGS:
        if(self.ccframe % fr_step == 0):  # 25 0.25s period
--- selfdrive/car/landrover/carstate.py
+++ selfdrive/car/landrover/carstate.py
@@ -2,9 +2,79 @@ import numpy as np
 from opendbc.can.parser import CANParser
 from opendbc.can.can_define import CANDefine
 from selfdrive.config import Conversions as CV
+from selfdrive.car.interfaces import CarStateBase
 from selfdrive.car.landrover.values import DBC, STEER_THRESHOLD
 from common.kalman.simple_kalman import KF1D
 from selfdrive.swaglog import cloudlog
+
+class CarState(CarStateBase):
+  def __init__(self, CP):
+    super().__init__(CP)
+    can_define = CANDefine(DBC[CP.carFingerprint]['pt'])
+    self.shifter_values = can_define.dv["GEAR"]['PRNDL']
+
+  def update(self, cp, cp_cam):
+
+    ret = car.CarState.new_message()
+
+    # update prevs, update must run once per loop
+    ret.doorOpen = 0
+    ret.seatbeltUnlatched = (cp.vl["SEAT_BELT"]["SEAT_BELT_DRIVER"]  == 1)
+    ret.brakePressed = (cp.vl["CRUISE_CONTROL"]['DRIVER_BRAKE'] == 1) # human-only
+    ret.brake = 0
+    ret.brakeLights = ret.brakePressed
+
+    ret.gas = 0 # cp.vl["ACCELATOR_DRIVER"]['ACCELATOR_DRIVER']
+    ret.gasPressed = ret.gas > 1e-5 = self.pedal_gas
+
+    self.espDisabled = 0 # (cp.vl["TRACTION_BUTTON"]['TRACTION_OFF'] == 1)
+
+    v_wheel = (cp.vl["SPEED_01"]["SPEED01"] + cp.vl["SPEED_02"]["SPEED02"]) / 2.
+
+    ret.v_wheel_fr = v_wheel # cp.vl["SPEED_04"]["WHEEL_SPEED_FR"]
+    ret.v_wheel_fl = v_wheel # cp.vl["SPEED_04"]["WHEEL_SPEED_FL"]
+    ret.v_wheel_rr = v_wheel # cp.vl["SPEED_03"]["WHEEL_SPEED_RR"]
+    ret.v_wheel_rl = v_wheel # cp.vl["SPEED_03"]["WHEEL_SPEED_RL"]
+    ret.vEgoRaw = v_wheel
+    ret.vEgo, ret.aEgo = self.update_speed_kf(ret.vEgoRaw)
+    ret.standstill = not ret.vEgoRaw > 0.001
+
+    ret.leftBlinker = cp.vl["TURN_SIGNAL"]['LEFT_TURN']
+    ret.rightBlinker = cp.vl["TURN_SIGNAL"]['RIGHT_TURN']
+
+    ret.gearShifter = self.parse_gear_shifter(self.shifter_values.get(cp.vl['GEAR']['PRNDL'], None))
+
+    ret.steeringAngle = float(cp.vl["EPS_01"]["STEER_ANGLE01"])
+    self.angle_steers_rate = (cp.vl["EPS_01"]["STEER_SPEED01"])
+    
+    # HANIL for landrover steers rate   
+    angle_steers_diff = float(self.angle_steers - self.prev_angle_steers) 
+
+    if angle_steers_diff < 0.0:
+           self.angle_rate_multi = -4
+    else:
+        if angle_steers_diff > 0.0:
+           self.angle_rate_multi = 4
+
+    self.angle_steers_rate *= self.angle_rate_multi
+    ret.steeringRate = self.angle_steers_rate
+
+
+    ret.cruiseState.enabled = (cp.vl["CRUISE_CONTROL"]['CRUISE_ON'] == 1)  # ACC is green.
+    ret.cruiseState.available = ret.cruiseState.enabled  # FIXME: for now same as enabled
+
+    ret.steeringTorque = cp.vl["EPS_03"]["STEER_TORQUE_DRIVER03"]
+    ret.steeringTtorqueEps = cp.vl["EPS_04"]["STEER_TORQUE_EPS04"]
+    ret.steeringPressed = abs(ret.steeringTorque) > STEER_THRESHOLD
+    
+    steer_state = 1 #cp.vl[""]["LKAS_STATE"]
+    ret.steerError = steer_state == 4 or (steer_state == 0 and ret.v_ego > self.CP.minSteerSpeed)
+    ret.genericToggle =  bool(cp.vl["HEAD_LIGHT"]["HIBEAM"] == 1)
+
+    self.lkas_counter = cp_cam.vl["LKAS_RUN"]['COUNTER']
+
+    return ret
+
 
 def parse_gear_shifter(can_gear):
   if can_gear == 0x0:
@@ -21,7 +91,6 @@ def parse_gear_shifter(can_gear):
 
 
 def get_can_parser(CP):
-
   signals = [
     # sig_name, sig_address, default
     ("STEER_RATE00", "EPS_00", 0),
@@ -77,6 +146,19 @@ def get_can_parser(CP):
 
   return CANParser(DBC[CP.carFingerprint]['pt'], signals, checks, 0)
 
+def parse_gear_shifter(can_gear):
+  if can_gear == 0x0:
+    return "park"
+  elif can_gear == 0x9:
+    return "reverse"
+  elif can_gear == 0x12:
+    return "neutral"
+  elif can_gear == 0x1b:
+    return "drive"
+  elif can_gear == 0xbb:
+    return "sport"
+  return "unknown"
+
 def get_camera_parser(CP):
   signals = [
     # sig_name, sig_address, default
@@ -86,104 +168,3 @@ def get_camera_parser(CP):
   checks = []
 
   return CANParser(DBC[CP.carFingerprint]['pt'], signals, checks, 2)
-
-class CarState():
-  def __init__(self, CP):
-    self.CP = CP
-    # self.can_define = CANDefine(DBC[CP.carFingerprint]['pt'])
-    self.left_blinker_on = 0
-    self.right_blinker_on = 0
-    # self.left_alert = 0
-    # self.right_alert = 0
-    
-    self.angle_steers = 0.0
-
-    # initialize can parser
-    self.car_fingerprint = CP.carFingerprint
-
-    # vEgo kalman filter
-    dt = 0.01
-    # Q = np.matrix([[10.0, 0.0], [0.0, 100.0]])
-    # R = 1e3
-    self.v_ego_kf = KF1D(x0=[[0.0], [0.0]],
-                         A=[[1.0, dt], [0.0, 1.0]],
-                         C=[1.0, 0.0],
-                         K=[[0.12287673], [0.29666309]])
-    self.v_ego = 0.0
-
-    self.prev_angle_steers = 0.0
-    self.angle_rate_multi = 1.0
-
-  def update(self, cp, cp_cam):
-
-    # update prevs, update must run once per loop
-    self.door_all_closed = 1
-    self.prev_left_blinker_on = self.left_blinker_on
-    self.prev_right_blinker_on = self.right_blinker_on
-    
-    self.prev_angle_steers = float(self.angle_steers)
-
-    self.seatbelt = (cp.vl["SEAT_BELT"]["SEAT_BELT_DRIVER"]  == 1)
-
-    self.brake_pressed = (cp.vl["CRUISE_CONTROL"]['DRIVER_BRAKE'] == 1) # human-only
-    
-    self.pedal_gas = 0 # cp.vl["ACCELATOR_DRIVER"]['ACCELATOR_DRIVER']
-    self.car_gas = self.pedal_gas
-    
-    self.esp_disabled = 0 # (cp.vl["TRACTION_BUTTON"]['TRACTION_OFF'] == 1)
-
-    v_wheel = (cp.vl["SPEED_01"]["SPEED01"] + cp.vl["SPEED_02"]["SPEED02"]) / 2.
-
-    self.v_wheel_fr = v_wheel # cp.vl["SPEED_04"]["WHEEL_SPEED_FR"]
-    self.v_wheel_fl = v_wheel # cp.vl["SPEED_04"]["WHEEL_SPEED_FL"]
-    self.v_wheel_rr = v_wheel # cp.vl["SPEED_03"]["WHEEL_SPEED_RR"]
-    self.v_wheel_rl = v_wheel # cp.vl["SPEED_03"]["WHEEL_SPEED_RL"]
-
-    # Kalman filter
-    if abs(v_wheel - self.v_ego) > 2.0:  # Prevent large accelerations when car starts at non zero speed
-      self.v_ego_kf.x = [[v_wheel], [0.0]]
-
-    self.v_ego_raw = v_wheel
-    v_ego_x = self.v_ego_kf.update(v_wheel)
-    self.v_ego = float(v_ego_x[0])
-    self.a_ego = float(v_ego_x[1])
-    self.standstill = not v_wheel > 0.001
-
-    self.angle_steers = float(cp.vl["EPS_01"]["STEER_ANGLE01"])
-    self.angle_steers_rate = (cp.vl["EPS_01"]["STEER_SPEED01"])
-    
-    # HANIL for landrover steers rate   
-    angle_steers_diff = float(self.angle_steers - self.prev_angle_steers) 
-
-    if angle_steers_diff < 0.0:
-           self.angle_rate_multi = -4
-    else:
-        if angle_steers_diff > 0.0:
-           self.angle_rate_multi = 4
-
-    self.angle_steers_rate *= self.angle_rate_multi
-
-    self.gear_shifter = parse_gear_shifter(cp.vl['GEAR_PRND']['GEAR_SHIFT'])
-    self.main_on = (cp.vl["CRUISE_CONTROL"]['CRUISE_ON'] == 1)  # ACC is green.
-    self.left_blinker_on = cp.vl["TURN_SIGNAL"]['LEFT_TURN']
-    self.right_blinker_on = cp.vl["TURN_SIGNAL"]['RIGHT_TURN']
-
-    # self.left_alert = bool(cp.vl["LEFT_ALERT"]['LEFT_ALERT_1'])
-    # self.right_alert = bool(cp.vl["RIGHT_ALERT"]['RIGHT_ALERT_1'])
-
-    self.steer_torque_driver = cp.vl["EPS_03"]["STEER_TORQUE_DRIVER03"]
-    self.steer_torque_motor = cp.vl["EPS_04"]["STEER_TORQUE_EPS04"]
-    
-    self.steer_override = abs(self.steer_torque_driver) > STEER_THRESHOLD
-    # self.steer_override = False # abs(self.steer_torque_driver) > STEER_THRESHOLD
-    steer_state = 1 #cp.vl[""]["LKAS_STATE"]
-    self.steer_error = steer_state == 4 or (steer_state == 0 and self.v_ego > self.CP.minSteerSpeed)
-
-    self.user_brake = 0
-    self.brake_lights = self.brake_pressed
-    self.v_cruise_pcm =  round(cp.vl["CRUISE_CONTROL"]['SPEED_CRUISE_RESUME']) 
-    self.pcm_acc_status = self.main_on
-    self.generic_toggle =  (cp.vl["HEAD_LIGHT"]["HIBEAM"] == 1)
-    self.lkas_counter = cp_cam.vl["LKAS_RUN"]['COUNTER']
-
-
